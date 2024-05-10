c6f7ce8de931f4c70754ee33e6d94f7a220388b1
sohucw <sohucw@users.noreply.github.com>
2024-05-10 01:01:00 +0000

    deploy: c499fffbefe85710ca8faaf7f80e81e0cb666103

---
+++ assets/AboutView-D6K8eE6t.js
@@ -0,0 +1,2 @@
+import{_ as e,o,c as s,a as t}from"./index-CFNyNHOU.js";const c={},a={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return o(),s("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
+//# sourceMappingURL=AboutView-D6K8eE6t.js.map
