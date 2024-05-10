9593a67676848284efabbd98bad1df67aa9721bb
dawei <chenjianwei01@baidu.com>
2024-05-11 07:59:29 +0800

    feat: 美化代码显示

--- src/views/PreView.vue
+++ src/views/PreView.vue
@@ -9,7 +9,7 @@
 export default {
     name: 'PreView',
     props: {
-        origin: Object
+        orgin: Object
     },
     methods: {
         preLine(){
@@ -18,7 +18,7 @@ export default {
             // 先获取源码有多少行
             const originCodeLine = this.orgin.source.split('\n')
             const len = originCodeLine.length - 1
-            const start = this.orgin.line - 3 >= 0 ? this.orgin.line - 3 : 0
+            const start = line- 3 >= 0 ? line - 3 : 0
             const end = start + 5 >= len ? len : start + 5 // 最多展示6行
             let newLines = []
             for (var i = start; i <= end; i++) {
