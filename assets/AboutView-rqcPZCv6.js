1fbbd3489b278f0cbf79f5c0fbca343957aa9244
sohucw <sohucw@users.noreply.github.com>
2024-05-11 00:49:43 +0000

    deploy: b2eea1a28b1a87b3c32e53f559bca23d44ee0fb9

--- assets/AboutView-rqcPZCv6.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o,c as s,a as t}from"./index-DVI8u0dh.js";const c={},a={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return o(),s("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
-//# sourceMappingURL=AboutView-rqcPZCv6.js.map
